be75d15e1625ece0fbb384e4b96a5aa50a9e6ab4
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2026-03-12 10:14:19 -0300

    model e migration de user e subscriber feitos

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Subscriber extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'email',
+    ];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
