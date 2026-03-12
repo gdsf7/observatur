22310932eb13eb922f7b6a4617121e939cd9cca7
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2026-03-12 09:41:09 -0300

    Microajustes no css da topbar

--- resources/js/Components/Main/TopBar.jsx
+++ resources/js/Components/Main/TopBar.jsx
@@ -3,15 +3,15 @@ import logo_caminhos from "../../../../public/images/logo_caminhos.png";
 
 export default function TopBar() {
     return (
-        <nav className="fixed top-0 left-0 w-full z-50 bg-white/95 backdrop-blur-sm shadow-md transition-all duration-300 opacity-98">
-            <div className="max-w-[1700px] h-20 mx-auto flex items-center justify-between px-6 lg:px-16 py-3">
+        <nav className="fixed top-0 left-0 w-full z-50 bg-gray-300 backdrop-blur-sm shadow-md transition-all duration-300 opacity-90">
+            <div className="max-w-[1700px] h-20 mx-auto flex ites-center justify-between px-6 lg:px-16 py-3">
                 
                 <div>
                     <Link href="/">
                         <img 
                             src={logo_caminhos} 
                             alt="Logo Caminhos do Sul" 
-                            className="h-16 md:h-16 w-auto object-contain"
+                            className="h-16 md:h-14 w-auto object-contain"
                         />
                     </Link>
                 </div>
