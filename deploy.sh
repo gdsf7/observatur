fa5cb1bfd7c2861cf6a10013bc6c3506033892f9
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2026-05-13 10:32:30 -0300

    alterada a ordem do deploy

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ docker compose up --build -d --remove-orphans
 docker compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader
 docker compose exec app npm install
 docker compose exec app php artisan ziggy:generate
+docker compose exec app php artisan migrate:fresh --seed --force
 docker compose exec app npm run build
 docker compose exec app php artisan cache:clear
 docker compose exec app php artisan view:clear
 docker compose exec app php artisan route:clear
 docker compose exec app php artisan config:clear
 
-docker compose exec app php artisan migrate:fresh --seed --force
 
 docker compose exec app php artisan config:cache
 docker compose exec app php artisan route:cache
