55de0d0f41fa01024a062b32d134ddca98e8d623
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2026-05-13 09:48:37 -0300

    Implementados a conteinerização com Docker e Nginx e pipeline de implantação automatizado do GitHub Actions

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+set -e 
+git checkout main
+git reset --hard HEAD
+git pull origin main
+cp ../.env.production .env
+docker compose up --build -d --remove-orphans
+docker compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader
+docker compose exec app npm install
+docker compose exec app php artisan ziggy:generate
+docker compose exec app npm run build
+docker compose exec app php artisan cache:clear
+docker compose exec app php artisan view:clear
+docker compose exec app php artisan route:clear
+docker compose exec app php artisan config:clear
+
+docker compose exec app php artisan migrate:fresh --seed --force
+
+docker compose exec app php artisan config:cache
+docker compose exec app php artisan route:cache
