48730c00bfa404b205d29e64018e284d28b4e62a
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2026-05-13 10:40:53 -0300

    de novo

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ git pull origin main
 cp ../.env.production .env
 docker compose up --build -d --remove-orphans
 docker compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader
+docker compose exec app php artisan migrate:fresh --seed --force
 docker compose exec app npm install
 docker compose exec app php artisan ziggy:generate
-docker compose exec app php artisan migrate:fresh --seed --force
 docker compose exec app npm run build
 docker compose exec app php artisan cache:clear
 docker compose exec app php artisan view:clear
