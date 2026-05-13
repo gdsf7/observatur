160ae7c69013caa0dae4622f6cac62f9578eea13
Gustavo Ferreira <gustavo.dsferreira05@gmail.com>
2026-05-13 10:49:19 -0300

    s

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ git pull origin main
 cp ../.env.production .env
 docker compose up --build -d --remove-orphans
 docker compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader
-docker compose exec app php artisan migrate:fresh --seed --force
 docker compose exec app npm install
 docker compose exec app php artisan ziggy:generate
 docker compose exec app npm run build
@@ -13,6 +12,7 @@ docker compose exec app php artisan cache:clear
 docker compose exec app php artisan view:clear
 docker compose exec app php artisan route:clear
 docker compose exec app php artisan config:clear
+docker compose exec app php artisan migrate:fresh --seed --force
 
 
 docker compose exec app php artisan config:cache
